e315f63577a8d69bbf978b22d4bf095a5e4d0717
fabiovalinhos <fabiovalinhos@gmail.com>
2023-10-12 20:04:04 -0300

    Criando as interfaces do Serviço

---
+++ src/ControleFacil.Api/Domain/Services/Classes/UsuarioService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFacil.Api.Contract.Usuario;
+using ControleFacil.Api.Domain.Repository.Interfaces;
+using ControleFacil.Api.Domain.Services.Interfaces;
+
+namespace ControleFacil.Api.Domain.Services.Classes
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            this._usuarioRepository = usuarioRepository;
+        }
+
+        public Task<UsuarioResponseContract> Adicionar(UsuarioRequestContract entidade, long idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioResponseContract> Atualizar(long id, UsuarioRequestContract entidade, long idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioLoginResponseContract> Autenticar(UsuarioLoginRequestContract usuarioLoginRequestContract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Inativar(long id, long idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UsuarioResponseContract>> Obter(long idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioResponseContract> Obter(long id, long idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
